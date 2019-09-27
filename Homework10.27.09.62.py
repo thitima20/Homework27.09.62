d67a1dcdfe4a40fe7bfdd7085107580a50404d49
thitima20 <52314903+thitima20@users.noreply.github.com>
2019-09-28 03:23:15 +0700

    Add files via upload

---
+++ Homework10.27.09.62.py
@@ -0,0 +1,6 @@
+"""
+ซื้อเสื้อผ้าราคาตัวละ 99 บาท 3 ตัว เสื้อกางเกงตัวละ 79 บาท 1 ตัว ต้องจ่ายเงินกี่บาท
+"""
+s=int(input("price shirts:"))
+p=int(input("price pants:"))
+print("pay",int(s*3+p),"bath")
