83a46dc7d3a50cc59a867dad3e57e9058cc52768
thitima20 <52314903+thitima20@users.noreply.github.com>
2019-09-28 03:22:08 +0700

    Add files via upload

---
+++ Homework8.27.09.62.py
@@ -0,0 +1,6 @@
+"""
+ซื้อโทรศัพท์ราคา 10000.50 บาท ขายไปและขาดทุน 0.005 ของราคาโทรศัพท์  อยากทราบว่าขายโทรศัพท์ไปราคากี่บาท
+"""
+Price = float(input("price : "))
+Sale  = float(input("a loos : "))
+print("sell phone price",int(Price*Sale),"bath")
